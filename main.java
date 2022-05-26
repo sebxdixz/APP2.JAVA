3b5c2c198ac2285cd8ce111ff9bb8bc35e106186
sebxdixz <sdiazdelafuente9@gmail.com>
2022-05-26 13:01:43 -0400

    Se actualiza el main.

---
+++ main.java
@@ -0,0 +1,9 @@
+//App 2: Editor de texto .csv 
+//La Aplicaion constara un menu el cual permitira al usuario seleccionar la opcion que desee
+//El Archivo CSV esta organizado como: titulo,autor,anio,estante_numero,estante_seccion,piso,edificio,sede
+//Opcion 1: Agregar libro nuevo.
+//Opcion 2: Quitar libro existente.
+//Opcion 3: Agregar nueva sede o cambiar de sede. (En un libro existen o en un libro nuevo)
+//Opcion 4: Quitar sede existente. (En un libro existen)
+//Opcion 5: Editar libro existente. En donde puede cambiar el titulo, autor, anio, estante, seccion, piso, edificio o sede.
+//Opcion 6: Salir. (Guardar todos los cambios)
